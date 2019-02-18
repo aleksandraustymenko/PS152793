28871826280aa1b2ca8260161d59ec950963f2c6
aleksandraustymenko <46462352+aleksandraustymenko@users.noreply.github.com>
2019-02-18 12:12:39 +0100

    Add files via upload

---
+++ 1_2_6/1_2_6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int liczba,var,const1;
+    scanf("%i",&liczba);
+    printf("%i\n",liczba);
+    scanf("%i",&var);
+    printf("%i\n",var);
+    scanf("%i",&const1);
+    printf("%i\n",const1);
+    return 0;
+}
