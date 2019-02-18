60ba8cfe092d99905013be8787b455df1b908b8c
aleksandraustymenko <46462352+aleksandraustymenko@users.noreply.github.com>
2019-02-18 12:13:18 +0100

    Add files via upload

---
+++ 1_2_8/1_2_8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    scanf("%i",&a);
+    printf("%i",a);
+    scanf("%i",&b);
+    printf("%i",b);
+    scanf("%i",&c);
+    printf("%i",c);
+    printf("%f",(double) (a+b+c)/3);
+    return 0;
+}
