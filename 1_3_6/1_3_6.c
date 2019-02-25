3a61e8996949261519e370e44fdcbcf2c518f512
aleksandraustymenko <46462352+aleksandraustymenko@users.noreply.github.com>
2019-02-25 12:47:47 +0100

    Add files via upload

---
+++ 1_3_6/1_3_6.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,c,d,e,m;
+    int x,y,Wx,Wy,W;
+    scanf("%i",&a);
+    scanf("%i",&b);
+    scanf("%i",&c);
+    scanf("%i",&d);
+    scanf("%i",&e);
+    scanf("%i",&m);
+    printf("%ix %iy=%i \n %ix %iy=%i",a,b,c,d,e,m);
+    W=a*e-b*d;
+    Wx=c*e-a*m;
+    Wy=a*m-c*d;
+    x=Wx/W;
+    y=Wy/W;
+    if(a==0 && b==0 && c==0 && d==0 && e==0 && m==0)
+    {
+        printf("Uklad jest sprzeczny\n");
+    }
+    if(W!=0)
+   {
+       if(Wx==0 && Wy==0)
+        {
+             printf("Uklad ma wielu rozwiazan\n");
+        }
+
+    else
+    {
+        printf("x=%f ,y=%f \n",x,y);
+    }
+   }
+   if(W==0)
+   {
+       printf("Wyznacznik rowny 0\n");
+   }
+
+    return 0;
+}
