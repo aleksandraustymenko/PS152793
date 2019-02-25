0eeef0352c4432aa7f85eb0cf3462310f668a2ab
aleksandraustymenko <46462352+aleksandraustymenko@users.noreply.github.com>
2019-02-25 12:47:27 +0100

    Add files via upload

---
+++ 1_3_5/1_3_5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int a,h,s,P,bok1,bok2,bok3;
+    printf("Pole trojkata:\n");
+    printf("Podaj dligosc i wysokosc:\n");
+    printf("Podaj dlugosc trzech bokow:\n");
+    scanf("%i",&a);
+    if(a==1)
+    {
+        printf("Podaj wysokosc:\n");
+        scanf("%i",&h);
+        printf("Podaj dlugosc podstawy:\n");
+        scanf("%i",&bok1);
+        P=bok1*h/2;
+    }
+    else
+    {
+      printf("Podaj dlugosc 1 boku:\n");
+      scanf ("%i",&bok1);
+      printf("Podaj dlugosc 2 boku:\n");
+      scanf("%i",&bok2);
+      printf("Podaj dlugosc 3 boku:\n");
+      scanf("%i",&bok3);
+      s=(bok1+bok2+bok3)/2;
+      P=sqrt(s*(s-bok1)*(s-bok2)*(s-bok3));
+
+    }
+    printf("Pole trojkata:%i\n",P);
+
+    return 0;
+}
