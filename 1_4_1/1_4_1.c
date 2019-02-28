4b4e35b8459e9d14856316df70f8aa38ca368d1f
aleksandraustymenko <46462352+aleksandraustymenko@users.noreply.github.com>
2019-02-28 22:02:37 +0100

    Add files via upload

---
+++ 1_4_1/1_4_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int n,m;
+    printf("Podaj liczbe n:\n");
+    scanf("%i",&n);
+    printf("Podaj liczbe m:\n");
+    scanf("%i",&m);
+    for(int i=n;i<m;i+=n)
+         printf("%i\n",i);
+    return 0;
+}
