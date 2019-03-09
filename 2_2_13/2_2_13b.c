006b700e80098bf4dc7b4113ecd9a9fcb7f85022
aleksandraustymenko <46462352+aleksandraustymenko@users.noreply.github.com>
2019-03-09 14:34:19 -0600

    Add files via upload

---
+++ 2_2_13/2_2_13b.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+
+int pierw(unsigned int x)
+{
+    int p=0,k=x,sred;
+    while(k-p>1)
+    {
+        sred=(p+k)/2;
+        if(sred*sred<=x)
+            p=sred;
+        else
+            k=sred;
+    }
+    if(x<=1)
+        return k;
+    else
+        return p;
+}
+void wypisz(unsigned int n)
+{
+    int i,s;
+    for(i=1;i<=pierw(n);i++)
+    {
+        s=pierw(n-i*i);
+        if((i*i+s*s==n)&&(i<s))
+            printf("%i*%i+%i*%i=%i\n",i,i,s,s,n);
+    }
+}
+int main()
+{
+    unsigned n;
+    scanf("%i",&n);
+    printf("Test\n");
+    wypisz(n);
+    return 0;
+}
