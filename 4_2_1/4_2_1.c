70d32387dfe792948707fe3aef821a9fdecb7588
aleksandraustymenko <46462352+aleksandraustymenko@users.noreply.github.com>
2019-03-23 10:25:36 +0100

    Add files via upload

--- 4_2_1/4_2_1.c
+++ 4_2_1/4_2_1.c
@@ -1,44 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-//nadaje wartoœæ zero wszystkim elementom tablicy tab
 
-void funkcje(unsigned int n,int tab[n])
+//nadaje wartoœæ zero wszystkim elementom tablicy tab
+void funkcje(unsigned int n,int * tab )
 {
     for(int i=0;i<n;i++)
     {
         tab[i]=0;
         printf("%i",tab[i]);
-
     }
     printf("\n");
 }
 //zapisuje do kolejnych elementów tablicy wartoœci równe ich indeksom
-
-void funkcje1(unsigned int n,int tab[n])
+void funkcje1(unsigned int n,int * tab )
 {
-    for(int i=0;i<n;i++)
+   for(int i=0;i<n;i++)
     {
         tab[i]=i;
         printf("%i",tab[i]);
-
     }
     printf("\n");
 }
 //podwaja wartoœæ wszystkich elementów w tablicy tab
-void funkcje2(unsigned int n,int tab[n])
+
+void funkcje2(unsigned int n,int * tab)
 {
     for(int i=0;i<n;i++)
     {
         tab[i]*=2;
         printf("%i",tab[i]);
-
     }
     printf("\n");
 }
  /*do wszystkich komórek tablicy tab wstawia wartoœci bezwzglêdne
 ich pierwotnych wartoœci*/
-void funkcje3(unsigned int n,int tab[n])
+void funkcje3(unsigned int n,int * tab)
 {
     for(int i=0;i<n;i++)
     {
@@ -49,7 +46,6 @@ void funkcje3(unsigned int n,int tab[n])
         printf("%i",tab[i]);
     }
     printf("\n");
-
 }
 /*
 void funkcje3(unsigned int n,int tab[n])
@@ -60,7 +56,6 @@ void funkcje3(unsigned int n,int tab[n])
         printf("%i",tab[i]);
     }
     printf("\n");
-
 }
 */
 int main(void)
@@ -77,5 +72,5 @@ int main(void)
     funkcje2(n,tab);
     funkcje3(n,tab);
     getchar();
-    exit(EXIT_SUCCESS);
+    return EXIT_SUCCESS;
 }
