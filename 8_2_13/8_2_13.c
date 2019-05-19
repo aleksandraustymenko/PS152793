95c35f8fd832e86987bfd5ef9ddc6c4858ba7667
aleksandraustymenko <46462352+aleksandraustymenko@users.noreply.github.com>
2019-05-19 17:30:42 +0200

    Add files via upload

---
+++ 8_2_13/8_2_13.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void funkcja(FILE* plik,FILE* plik1)
+{
+    char ch;
+    while(fscanf(plik,"%c",&ch) == 1)
+    {
+        fprintf(plik1,"%c",&ch);
+    }
+}
+int main()
+{
+    funkcja(fopen("var1.txt","r"),fopen("var.txt","w"));
+    return 0;
+}
